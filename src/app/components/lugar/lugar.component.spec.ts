10b2af23be36dbf46fa34c90cb78e894ed626ba9
Jaiden Meiden <jaidenmeiden@gmail.com>
2019-10-16 22:21:31 -0500

    Se implementa el test para validar si se trae la información correcta

--- src/app/components/lugar/lugar.component.spec.ts
+++ src/app/components/lugar/lugar.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LugarComponent } from './lugar.component';
+import {LugaresService} from '../../services/lugares.service';
 
 describe('LugarComponent', () => {
   beforeEach(async(() => {
@@ -15,5 +16,12 @@ describe('LugarComponent', () => {
     const fixture = TestBed.createComponent(LugarComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should have the same places as the service', () => {
+    const fixture = TestBed.createComponent(LugarComponent);
+    const app = fixture.debugElement.componentInstance;
+    const lugaresService = fixture.debugElement.injector.get(LugaresService);
+    expect(app.lugares).toEqual(lugaresService.lugares);
   });
 });
