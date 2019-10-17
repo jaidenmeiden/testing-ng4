7f6d8070d67f3ef6800d1bf105b5b7b52bcb442d
Jaiden Meiden <jaidenmeiden@gmail.com>
2019-10-16 22:00:32 -0500

    Se crea compoente lugasr para hacer test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LugarComponent } from './components/lugar/lugar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LugarComponent
   ],
   imports: [
     BrowserModule
