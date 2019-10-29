f3b1a4feb5717dba616afb29d75512e0ea728616
DLI <sistemas@dli.com.co>
2019-10-29 09:28:15 -0500

    Se modifica nombre del proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platzi-test-ng4'`, () => {
+  it(`should have as title 'testing-ng4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('platzi-test-ng4');
+    expect(app.title).toEqual('testing-ng4');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to platzi-test-ng4!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to testing-ng4!');
   });
 });
